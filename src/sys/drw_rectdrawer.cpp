0c38cbc5d7a2033cee62007f5e00c8241bac1b7f
sponge <sponge@d8d.org>
2016-07-11 23:29:45 -0500

    draw pos and time on squares

--- src/sys/drw_rectdrawer.cpp
+++ src/sys/drw_rectdrawer.cpp
@@ -1,4 +1,5 @@
 #include "systems.h"
+#include <stdio.h>
 
 void RectDrawSystem::render(EntityManager &es, ClientInfo *inf) {
 	NVGcontext *nvg = inf->nvg;
@@ -11,6 +12,15 @@ void RectDrawSystem::render(EntityManager &es, ClientInfo *inf) {
 		nvgRect(nvg, body->x - (body->w*0.5), body->y - (body->h*0.5), body->w, body->h);
 		nvgFillColor(nvg, nvgRGBA(r->r, r->g, r->b, r->a));
 		nvgFill(nvg);
+
+		nvgFillColor(nvg, nvgRGBA(255, 255, 255, 255));
+		char s[16];
+		auto m = ent.component<Movable>();
+		nvgTextAlign(nvg, 2);
+		snprintf(s, sizeof(s), "%.0f, %.0f", body->x, body->y);
+		nvgText(nvg, body->x, body->y, s, 0);
+		snprintf(s, sizeof(s), "%.0f, %.0f", m->dx, m->dy);
+		nvgText(nvg, body->x, body->y + 12, s, 0);
 	}
 }
 

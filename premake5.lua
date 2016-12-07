2345afdf342e37bccdc793a92975ff944cc81f2d
sponge <sponge@d8d.org>
2016-12-06 20:29:27 -0600

    clean up premake file a bit. spacing, extra defines, move debug/release up to top

--- premake5.lua
+++ premake5.lua
@@ -1,69 +1,60 @@
 solution "game"
-   configurations { "Debug", "Release" }
-   location "build"
-   platforms {"x86", "x64"}
+  configurations { "Debug", "Release" }
+  location "build"
+  platforms {"x86", "x64"}
 
-   project "game"
-      kind "ConsoleApp"
-      language "C++"
-      files { "src/**.c", "src/**.cpp", "src/**.h", "src/**.hh" }
-      sysincludedirs { "include", "nanovg", "tmx", "imgui", "physfs" }
-      debugdir "."
-      targetdir "bin/%{cfg.buildcfg}"
-      links { "nanovg", "tmx", "imgui", "physfs", "glew" }
+  configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols", "ExtraWarnings" }
 
-      configuration { "windows" }
-         libdirs { "lib/Win32" }
-         links { "SDL2", "SDL2main", "glew32s", "opengl32" }
-         defines { "_CRT_SECURE_NO_WARNINGS" }
+  configuration "Release"
+    defines { "NDEBUG" }
+    flags { "Optimize", "ExtraWarnings" }
 
-         postbuildcommands {
-            '{COPY} "%{wks.location}../lib/win32/SDL2.dll" "%{cfg.targetdir}"'
-         }
+  project "game"
+    kind "ConsoleApp"
+    language "C++"
+    files { "src/**.c", "src/**.cpp", "src/**.h", "src/**.hh" }
+    sysincludedirs { "include", "nanovg", "tmx", "imgui", "physfs" }
+    debugdir "."
+    targetdir "bin/%{cfg.buildcfg}"
+    links { "nanovg", "tmx", "imgui", "physfs", "glew" }
 
-      configuration { "macosx" }
-         links { "OpenGL.framework", "SDL2.framework", "CoreFoundation.framework", "IOKit.framework", "CoreServices.framework", "Cocoa.framework" }
-         buildoptions {"-std=c++14", "-stdlib=libc++"}
-         linkoptions {"-stdlib=libc++", "-F /Library/Frameworks"}
-
-      configuration "Debug"
-         defines { "DEBUG" }
-         flags { "Symbols", "ExtraWarnings" }
-
-      configuration "Release"
-         defines { "NDEBUG" }
-         flags { "Optimize", "ExtraWarnings" }
-
-	project "nanovg"
-		language "C"
-		kind "StaticLib"
-		includedirs { "nanovg" }
-		files { "nanovg/**.c", "nanovg/**.h" }
-		targetdir "build"
-		defines { "_CRT_SECURE_NO_WARNINGS" }
-
-   project "tmx"
-      language "C++"
-      kind "StaticLib"
-      sysincludedirs { "include" }
-      files { "tmx/**.c", "tmx/**.h", "tmx/**.cpp" }
-      targetdir "build"
+    configuration { "windows" }
+      libdirs { "lib/Win32" }
+      links { "SDL2", "SDL2main", "opengl32" }
       defines { "_CRT_SECURE_NO_WARNINGS" }
-      buildoptions {"-std=c++14", "-stdlib=libc++"}
-		
-		configuration "Debug"
-			defines { "DEBUG" }
-			flags { "Symbols", "ExtraWarnings"}
+      postbuildcommands {
+        '{COPY} "%{wks.location}../lib/win32/SDL2.dll" "%{cfg.targetdir}"'
+      }
 
-		configuration "Release"
-			defines { "NDEBUG" }
-			flags { "Optimize", "ExtraWarnings"}
+    configuration { "macosx" }
+      links { "OpenGL.framework", "SDL2.framework", "CoreFoundation.framework", "IOKit.framework", "CoreServices.framework", "Cocoa.framework" }
+      buildoptions {"-std=c++14", "-stdlib=libc++"}
+      linkoptions {"-stdlib=libc++", "-F /Library/Frameworks"}
+
+  project "nanovg"
+    language "C"
+    kind "StaticLib"
+    includedirs { "nanovg" }
+    files { "nanovg/**.c", "nanovg/**.h" }
+    targetdir "build"
+    defines { "_CRT_SECURE_NO_WARNINGS" }
+
+  project "tmx"
+    language "C++"
+    kind "StaticLib"
+    sysincludedirs { "include" }
+    files { "tmx/**.c", "tmx/**.h", "tmx/**.cpp" }
+    targetdir "build"
+    defines { "_CRT_SECURE_NO_WARNINGS" }
+    buildoptions {"-std=c++14", "-stdlib=libc++"}
 
   project "imgui"
-	  language "C++"
-	  kind "StaticLib"
-	  files { "imgui/**.cpp", "imgui/**.h" }
-      targetdir "build"
+    language "C++"
+    kind "StaticLib"
+    files { "imgui/**.cpp", "imgui/**.h" }
+    targetdir "build"
 
   project "physfs"
     language "C"

6b8b49a0e5c872ed25183742624275d7a78e35ec
sponge <sponge@d8d.org>
2016-12-09 07:32:01 -0600

    more cleanup and porting

--- src/scene_testbounce.cpp
+++ src/scene_testbounce.cpp
@@ -4,36 +4,11 @@
 #include <tmx.h>
 #include <entityx/entityx.h>
 
-//#include "sys/systems.h"
+#include "sys/systems_drw.h"
+//#include "sys/systems_upd.h"
 #include "components.h"
 
 namespace ex = entityx;
-
-class RectDrawSystem : public ex::System<RectDrawSystem> {
-public:
-  explicit RectDrawSystem(ClientInfo *inf) : inf(inf) {}
-
-  void update(ex::EntityManager &es, ex::EventManager &events, ex::TimeDelta dt) override {
-		NVGcontext *nvg = inf->nvg;
-
-		es.each<Body, Renderable, Movable>([this, nvg](ex::Entity entity, Body &body, Renderable &r, Movable &m) {
-			nvgBeginPath(nvg);
-			nvgRect(nvg, body.pos.x - (body.size.x*0.5), body.pos.y - (body.size.y*0.5), body.size.x, body.size.y);
-			nvgFillColor(nvg, nvgRGBA(r.r, r.g, r.b, r.a));
-			nvgFill(nvg);
-
-			nvgFillColor(nvg, nvgRGBA(255, 255, 255, 255));
-			char s[16];
-			nvgTextAlign(nvg, 2);
-			snprintf(s, sizeof(s), "%.0f, %.0f", body.pos.x, body.pos.y);
-			nvgText(nvg, body.pos.x, body.pos.y, s, 0);
-			snprintf(s, sizeof(s), "%.0f, %.0f", m.dx, m.dy);
-			nvgText(nvg, body.pos.x, body.pos.y + 12, s, 0);
-		});
-  }
-private:
-	ClientInfo *inf;
-};
 
 class TestBounceWorld : public ex::EntityX {
 public:
---
+++ src/sys/systems_upd.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <entityx/entityx.h>
+#include "../local.h"
+
+namespace ex = entityx;

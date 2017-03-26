621b0fd964ac066584ba92f92c4fb6ed0645ca98
sponge <sponge@d8d.org>
2017-03-26 00:20:37 -0500

    get module loading from physfs working

--- game/lua_extstate.cpp
+++ game/lua_extstate.cpp
@@ -3,6 +3,38 @@
 
 LuaExt lua;
 
+static int physfs_searcher(lua_State* st) {
+	const char* file = lua_tostring(st, 1);
+
+	const char *buffer;
+	int sz;
+
+	sz = trap->FS_ReadFile(va("scripts/%s", file), (void **)&buffer);
+	if (sz == -1) {
+		sz = trap->FS_ReadFile(va("scripts/%s.lua", file), (void **)&buffer);
+	}
+
+	if (sz == -1) {
+		return 0;
+	}
+
+	auto status = luaL_loadbuffer(st, buffer, sz, file);
+
+	if (status != LUA_OK) {
+		if (status == LUA_ERRSYNTAX) {
+			const char *msg = lua_tostring(st, -1);
+			trap->Print(msg ? msg : va("%s: syntax error", file));
+		}
+		else if (status == LUA_ERRFILE) {
+			const char *msg = lua_tostring(st, -1);
+			trap->Print(msg ? msg : va("%s: file error", file));
+		}
+		return 0;
+	}
+
+	return 1;
+}
+
 static int l_my_print(lua_State* L) {
 	int nargs = lua_gettop(L);
 
@@ -26,16 +58,18 @@ LuaExt::LuaExt() {
 
 	static const struct luaL_Reg printlib[] = {
 		{ "print", l_my_print },
+		{ "fs_require", physfs_searcher},
 		{ NULL, NULL }
 	};
 
 	lua_getglobal(st, "_G");
 	luaL_setfuncs(st, printlib, 0);
 	lua_pop(st, 1);
+
+	lua.script("table.insert(package.searchers, 1, fs_require)");
 }
 
 bool LuaExt::LoadGameFile(const std::string &file) {
-
 	auto st = this->lua_state();
 
 	const char *buffer;

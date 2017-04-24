2957f78e3e26f52a33b23a27f1bd7a0d37659d73
sponge <sponge@d8d.org>
2017-04-24 07:30:28 -0500

    cleanup some lua

--- base/scripts/main.lua
+++ base/scripts/main.lua
@@ -13,16 +13,12 @@ spawn_entity = function(world, obj, props)
         local camera = world:getCamera(world.master_entity.id)
 
         local ent = world:new_entity()
-        local b = Body:new(obj.x + (map.tile_width / 2), obj.y - 14.001, 16, 15)
-        world:addBody(ent, b)
+        world:addBody(ent, Body:new(obj.x + (map.tile_width / 2), obj.y - 14.001, 16, 15))
         world:addMovable(ent, Movable:new(0, 0))
         world:addRenderable(ent, Renderable:new(200, 30, 30, 200))
         world:addPlayerInput(ent, PlayerInput:new())
-        local playerImg = world:new_image("player", "gfx/dog.png")
-        world:addSprite(ent, Sprite:new(playerImg, 22, 15, 0, 0))
-
+        world:addSprite(ent, Sprite:new(world:new_image("player", "gfx/dog.png"), 22, 15, 0, 0))
         world:addTable(ent, {
-            hello = 69,
             num_jumps = 0,
             is_wall_sliding = false,
             can_wall_jump = false,
@@ -30,18 +26,16 @@ spawn_entity = function(world, obj, props)
             will_pogo = false,
             stunTime = 0.0
         })
-
         world:add_entity(ent)
 
         camera.target = ent.id
 
     elseif obj.type == 'goal' then
         local map = world:getTileMap(world.master_entity).map
+
         local ent = world:new_entity()
-        local b = Body:new(obj.x, obj.y, 32, 32)
-        world:addBody(ent, b)
-        local bone = world:new_image("bone", "gfx/bone.png")
-        world:addSprite(ent, Sprite:new(bone, 32, 32, 0, 0))
+        world:addBody(ent, Body:new(obj.x, obj.y, 32, 32))
+        world:addSprite(ent, Sprite:new(world:new_image("bone", "gfx/bone.png"), 32, 32, 0, 0))
         world:addAnimation(ent, Animation:new(0, 0, 7, 0.1, world.time))
         world:add_entity(ent)
 
@@ -72,11 +66,8 @@ world:add_system {
 world:add_system {
     name = "Player Update",
     priority = 0,
-    components = {COMPONENT_PLAYERINPUT, COMPONENT_BODY, COMPONENT_MOVABLE, COMPONENT_LUATABLE},
+    components = {COMPONENT_PLAYERINPUT, COMPONENT_BODY, COMPONENT_MOVABLE, COMPONENT_LUATABLE, COMPONENT_SPRITE},
     process = function(dt, ent, c)
-        c.table.hello = c.table.hello + 1
-
-
         if c.playerinput.right then
             c.mov.dx = 50 * dt
         elseif c.playerinput.left then
@@ -93,16 +84,20 @@ world:add_system {
             c.mov.dy = 0
         end
 
+        if c.mov.dx ~= 0 then
+            c.sprite.flipX = c.mov.dx < 0 and true or false
+        end
+
         local xmove = world:trace(ent, c.mov.dx, 0)
         c.body.x = xmove.pos.x
-        -- if xmove.hit.valid then
-        --     c.mov.dx = 0
-        -- end
+        if xmove.hit.valid then
+            c.mov.dx = 0
+        end
 
         local ymove = world:trace(ent, 0, c.mov.dy)
         c.body.y = ymove.pos.y
-        -- if ymove.hit.valid then
-        --     c.mov.dy = 0
-        -- end
+        if ymove.hit.valid then
+            c.mov.dy = 0
+        end
     end
 }

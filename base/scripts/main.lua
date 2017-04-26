a0a74798378e1614ea8f20d417208f7872a138d4
sponge <sponge@d8d.org>
2017-04-25 19:36:27 -0500

    get rid of all the c.component references to clean it up a bit

--- base/scripts/main.lua
+++ base/scripts/main.lua
@@ -73,13 +73,18 @@ world:add_system {
     components = {COMPONENT_PLAYERINPUT, COMPONENT_BODY, COMPONENT_MOVABLE, COMPONENT_LUATABLE, COMPONENT_SPRITE, COMPONENT_ANIMATION},
     process = function(dt, ent, c)
         local player = c.table
+        local mov = c.mov
+        local input = c.input
+        local spr = c.sprite
+        local anim = c.animation
+        local body = c.body
 
         local right_touch = world:trace(ent, 1, 0).time < 1e-7;
         local left_touch = world:trace(ent, -1, 0).time < 1e-7;
         local down_touch = world:trace(ent, 0, 1).time < 1e-7;
         local up_touch = world:trace(ent, 0, -1).time < 1e-7;
 
-        world:debug_text(inspect(c.table))
+        world:debug_text(inspect(player))
         world:debug_text("l:" .. tostring(left_touch) .. " r:".. tostring(right_touch) .. " d:".. tostring(down_touch) .. " u:" .. tostring(up_touch))
 
         -- if they're on the ground, they can always jump
@@ -90,46 +95,46 @@ world:add_system {
         player.is_wall_sliding = false
 
         -- if they're midair and are holding down toward the wall, they can wall jump
-        player.can_wall_jump = (not down_touch) and (c.input.left and left_touch) or (c.input.right and right_touch)
+        player.can_wall_jump = (not down_touch) and (input.left and left_touch) or (input.right and right_touch)
 
         -- if they're moving down and touching a wall the direction they're holding down, they are wall sliding
-        if c.mov.dy > 0 and player.can_wall_jump then
+        if mov.dy > 0 and player.can_wall_jump then
             player.is_wall_sliding = true
         end
 
         -- FIXME: is this right?
-        if down_touch and (c.mov.dy < 0 or not player.will_pogo) then
-            player.will_pogo = c.input.down
+        if down_touch and (mov.dy < 0 or not player.will_pogo) then
+            player.will_pogo = input.down
         end
 
         -- finished updating player, now start figuring out speeds
 
-        c.mov.dy = c.mov.dy + cvars["p_gravity"].value * dt
+        mov.dy = mov.dy + cvars["p_gravity"].value * dt
         
         -- apply wallslide speed after gravity
         if player.is_wall_sliding then
-            c.mov.dy = cvars["p_wallSlideSpeed"].value
+            mov.dy = cvars["p_wallSlideSpeed"].value
         end
 
         -- reset jump and slow upward velocity
-        if not c.input.jump and player.jump_held then
+        if not input.jump and player.jump_held then
             player.jump_held = false
-            if c.mov.dy < 0 then
-                c.mov.dy = c.mov.dy * cvars["p_earlyJumpEndModifier"].value
+            if mov.dy < 0 then
+                mov.dy = mov.dy * cvars["p_earlyJumpEndModifier"].value
             end
         end
 
         -- if touching ground and are about to pogo, pogo is first prio
         if down_touch and player.will_pogo then
-            c.mov.dy = 0 - cvars["p_pogoJumpHeight"].value
+            mov.dy = 0 - cvars["p_pogoJumpHeight"].value
             player.num_jumps = 1
             player.will_pogo = false
         -- check for various types of jumps
-        elseif c.input.jump and not player.jump_held then
+        elseif input.jump and not player.jump_held then
             -- wall jump
             if player.can_wall_jump then
-                c.mov.dy = 0 - cvars["p_doubleJumpHeight"].value
-                c.mov.dx = cvars["p_wallJumpX"].value * (c.input.right and -1 or 1)
+                mov.dy = 0 - cvars["p_doubleJumpHeight"].value
+                mov.dx = cvars["p_wallJumpX"].value * (input.right and -1 or 1)
                 player.stun_time = world.time + 0.2
                 player.jump_held = true
                 player.num_jumps = 1
@@ -137,69 +142,76 @@ world:add_system {
             -- regular on ground jump
             elseif down_touch then
                 -- FIXME: mov.dy = -(p_jumpHeight->value + (fabs(mov.dx) >= p_maxSpeed->value * 0.25f ? p_speedJumpBonus->value : 0));
-                c.mov.dy = -cvars["p_jumpHeight"].value
+                mov.dy = -cvars["p_jumpHeight"].value
                 player.jump_held = true
                 player.num_jumps = 1
 
             -- midair double jump
             elseif player.num_jumps < 2 then
-                c.mov.dy = -cvars["p_doubleJumpHeight"].value
+                mov.dy = -cvars["p_doubleJumpHeight"].value
                 player.num_jumps = 2
                 player.jump_held = true
             end
 
         end
 
-        if c.input.right or c.input.left then
+        -- moving in a direction
+        if input.right or input.left then
             local accel = 0;
-            local isSkidding = (c.input.left and c.mov.dx > 0) or (c.input.right and c.mov.dx < 0)
+            local isSkidding = (input.left and mov.dx > 0) or (input.right and mov.dx < 0)
+            -- player can't move in any direction
             if world.time < player.stun_time then
                 accel = 0
+            -- player has ground acceleration
             elseif down_touch then
                 accel = isSkidding and cvars["p_skidAccel"].value or cvars["p_accel"].value
+            -- player has air acceleration
             else
                 accel = isSkidding and cvars["p_turnAirAccel"].value or cvars["p_airAccel"].value
             end
             
-            if c.input.left then
+            if input.left then
                 accel = -accel
             end
-            c.mov.dx = c.mov.dx + accel * dt
-            c.sprite.flipX = not c.input.right
-        elseif c.mov.dx ~= 0 then
+
+            mov.dx = mov.dx + accel * dt
+            spr.flipX = not input.right
+
+        elseif mov.dx ~= 0 then
+            -- apply ground friction to bring to a stop
             local friction = cvars["p_groundFriction"].value * dt;
-            if friction > math.abs(c.mov.dx) then
-                c.mov.dx = 0;
+            if friction > math.abs(mov.dx) then
+                mov.dx = 0;
             else
-                c.mov.dx = c.mov.dx + friction * (c.mov.dx > 0 and -1 or 1);
+                mov.dx = mov.dx + friction * (mov.dx > 0 and -1 or 1);
             end
         end
 
-        c.mov.dx = clamp(c.mov.dx, -cvars["p_maxSpeed"].value, cvars["p_maxSpeed"].value);
-        local uncappedY = c.mov.dy;
-        c.mov.dy = clamp(c.mov.dy, -cvars["p_terminalVelocity"].value, cvars["p_terminalVelocity"].value);
+        mov.dx = clamp(mov.dx, -cvars["p_maxSpeed"].value, cvars["p_maxSpeed"].value);
+        local uncappedY = mov.dy;
+        mov.dy = clamp(mov.dy, -cvars["p_terminalVelocity"].value, cvars["p_terminalVelocity"].value);
 
         -- sprite and animation
-        if c.mov.dx ~= 0 then
-            if c.animation.endFrame == 0 then
-                c.animation.startTime = world.time
-                c.animation.endFrame = 6
+        if mov.dx ~= 0 then
+            if anim.endFrame == 0 then
+                anim.startTime = world.time
+                anim.endFrame = 6
             end
         else
-            c.animation.endFrame = 0
+            anim.endFrame = 0
         end
         
 
-        local xmove = world:trace(ent, c.mov.dx * dt, 0)
-        c.body.x = xmove.pos.x
+        local xmove = world:trace(ent, mov.dx * dt, 0)
+        body.x = xmove.pos.x
         if xmove.hit.valid then
-            c.mov.dx = 0
+            mov.dx = 0
         end
 
-        local ymove = world:trace(ent, 0, c.mov.dy * dt)
-        c.body.y = ymove.pos.y
+        local ymove = world:trace(ent, 0, mov.dy * dt)
+        body.y = ymove.pos.y
         if ymove.hit.valid then
-            c.mov.dy = 0
+            mov.dy = 0
         end
 
         local trigger_ent = world:check_trigger(ent)
@@ -210,17 +222,13 @@ world:add_system {
             print("triggered! ".. tostring(trig.type))
         end
 
-        if math.abs(c.mov.dx) < 0.2 then
-            c.mov.dx = 0;
-        end
-
-        if math.abs(c.mov.dy) < 0.2 then
-            c.mov.dy = 0;
+        if math.abs(mov.dy) < 0.2 then
+            mov.dy = 0;
         end
 
         -- keep going upward as long as we don't have an upward collision
-        if c.mov.dy < 0 and not ymove.hit.valid then
-            c.mov.dy = uncappedY;
+        if mov.dy < 0 and not ymove.hit.valid then
+            mov.dy = uncappedY;
         end
     end
 }

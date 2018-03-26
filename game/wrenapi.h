1168c9f1c5696e9b9a18b2b60721e94621492949
sponge <sponge@d8d.org>
2018-03-26 00:34:39 -0500

    expose eval to dll, but disable console because it breaks shit

--- game/wrenapi.h
+++ game/wrenapi.h
@@ -3,6 +3,7 @@
 struct WrenVM *Wren_Init(const char *mainScriptName, const char *constructorStr);
 void Wren_Update(WrenVM *vm, float dt);
 void Wren_Draw(struct WrenVM *vm, int w, int h);
+void Wren_Eval(WrenVM *vm, const char *code);
 void Wren_Scene_Shutdown(WrenVM *vm);
 void Wren_FreeVM(WrenVM *vm);
 

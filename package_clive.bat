ee226af42103d8d5d5362238a1b41eda7fc900fe
sponge <sponge@d8d.org>
2018-07-29 13:17:25 -0500

    engine.exe -> clive.exe, game.zip -> clive.zip

--- package_clive.bat
+++ package_clive.bat
@@ -6,7 +6,9 @@ copy build\bin\Release\engine.exe package\
 copy build\bin\Release\SDL2.dll package\
 copy build\bin\release\game.dll package\base\
 
+rename package\engine.exe clive.exe
+
 "c:\Program Files\7-Zip\7z.exe" a -tzip package\base\pak00.pk3 .\base\*
 "c:\Program Files\7-Zip\7z.exe" u -tzip package\base\pak00.pk3 .\clive\*
 
-"c:\Program Files\7-Zip\7z.exe" a -tzip package\game.zip .\package\*
+"c:\Program Files\7-Zip\7z.exe" a -tzip package\clive.zip .\package\*

fb769029cc07b2c872278acdff84e26df07d9f6e
sponge <sponge@d8d.org>
2018-07-28 23:20:07 -0500

    batch file for clive packaging

--- package_clive.bat
+++ package_clive.bat
@@ -7,6 +7,6 @@ copy build\bin\Release\SDL2.dll package\
 copy build\bin\release\game.dll package\base\
 
 "c:\Program Files\7-Zip\7z.exe" a -tzip package\base\pak00.pk3 .\base\*
-"c:\Program Files\7-Zip\7z.exe" u -tzip package\base\pak00.pk3 .\plat\*
+"c:\Program Files\7-Zip\7z.exe" u -tzip package\base\pak00.pk3 .\clive\*
 
 "c:\Program Files\7-Zip\7z.exe" a -tzip package\game.zip .\package\*

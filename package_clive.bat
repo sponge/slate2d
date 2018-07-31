f562bba7dff20f8348a80696ffd5b2fb84b46025
sponge <sponge@d8d.org>
2018-07-31 14:33:49 -0500

    package clive into a goat

--- package_clive.bat
+++ package_clive.bat
@@ -8,6 +8,8 @@ copy build\bin\release\game.dll package\base\
 
 rename package\engine.exe clive.exe
 
+rcedit-x64 "package\clive.exe" --set-icon "clive\goat.ico"
+
 "c:\Program Files\7-Zip\7z.exe" a -tzip package\base\pak00.pk3 .\base\*
 "c:\Program Files\7-Zip\7z.exe" u -tzip package\base\pak00.pk3 .\clive\*
 

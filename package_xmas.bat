226c70b66083190838ae132dcf687d69d764af06
sponge <sponge@d8d.org>
2018-12-31 21:39:01 -0600

    put a folder at the top level of the zip file

--- package_xmas.bat
+++ package_xmas.bat
@@ -1,16 +1,17 @@
 @echo off
 rmdir /S /Q package
 
-mkdir package\base
-copy build\bin\Release\engine.exe package\
-copy build\bin\Release\SDL2.dll package\
-copy build\bin\release\game.dll package\base\
+mkdir package\hotair
+mkdir package\hotair\base
+copy build\bin\Release\engine.exe package\hotair\
+copy build\bin\Release\SDL2.dll package\hotair\
+copy build\bin\release\game.dll package\hotair\base\
 
-rename package\engine.exe hotair.exe
+rename package\hotair\engine.exe hotair.exe
 
 rcedit-x64 "package\hotair.exe" --set-icon "xmas\icon.ico"
 
-"c:\Program Files\7-Zip\7z.exe" a -tzip package\base\pak00.pk3 .\base\*
-"c:\Program Files\7-Zip\7z.exe" u -tzip package\base\pak00.pk3 .\xmas\* -xr!.git -xr!.vscode
+"c:\Program Files\7-Zip\7z.exe" a -tzip package\hotair\base\pak00.pk3 .\base\*
+"c:\Program Files\7-Zip\7z.exe" u -tzip package\hotair\base\pak00.pk3 .\xmas\* -xr!.git -xr!.vscode
 
 "c:\Program Files\7-Zip\7z.exe" a -tzip package\hotair.zip .\package\*

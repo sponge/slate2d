dd7f82fce16c930aa01881528546144e88bde80c
sponge <sponge@d8d.org>
2019-01-06 10:11:12 -0600

    make package.bat take a param for mod name instead of separate files

--- package.bat
+++ package.bat
@@ -1,12 +1,16 @@
 @echo off
+set name=%1
+
 rmdir /S /Q package
 
-mkdir package\base
-copy build\bin\Release\engine.exe package\
-copy build\bin\Release\SDL2.dll package\
-copy build\bin\release\game.dll package\base\
+mkdir package\%name%\base
+copy build\bin\Release\engine.exe package\%name%\%name%.exe
+copy build\bin\Release\SDL2.dll package\%name%\
+copy build\bin\Release\game.dll package\%name%\base\
 
-"c:\Program Files\7-Zip\7z.exe" a -tzip package\base\pak00.pk3 .\base\*
-"c:\Program Files\7-Zip\7z.exe" u -tzip package\base\pak00.pk3 .\plat\*
+rcedit-x64 "package\%name%\%name%.exe" --set-icon "%name%\icon.ico"
 
-"c:\Program Files\7-Zip\7z.exe" a -tzip package\game.zip .\package\*
+"c:\Program Files\7-Zip\7z.exe" a -tzip package\%name%\base\pak00.pk3 .\base\*
+"c:\Program Files\7-Zip\7z.exe" u -tzip package\%name%\base\pak00.pk3 .\%name%\*
+
+"c:\Program Files\7-Zip\7z.exe" a -tzip package\%name%.zip .\package\*

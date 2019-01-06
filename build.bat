6d8d73c2aea492e5ff8bcce789119d3f40349985
sponge <sponge@d8d.org>
2019-01-06 09:56:27 -0600

    redo vscode tasks and launch to be cleaner and start being usable again

--- build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
-cd %1
-msbuild /property:GenerateFullPaths=true /t:build /p:Platform=Win32 /p:Configuration=Debug build/game.sln

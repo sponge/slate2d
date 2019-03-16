2f3a7eacda56c078bdaf99e8df21aaa49d881974
sponge <sponge@d8d.org>
2019-03-16 15:37:46 -0500

    accidentally broke package.bat

--- package.bat
+++ package.bat
@@ -13,7 +13,7 @@ copy build\bin\x86_64_Release\openmpt-vorbis.dll build\package\%name%\
 copy build\bin\x86_64_Release\openmpt-zlib.dll build\package\%name%\
 copy build\bin\x86_64_Release\game.dll build\package\%name%\base\
 
-if exist %name%\icon.ico "build\package\%name%\%name%.exe" --set-icon "%name%\icon.ico"
+if exist %name%\icon.ico rcedit-x64 "build\package\%name%\%name%.exe" --set-icon "%name%\icon.ico"
 
 "c:\Program Files\7-Zip\7z.exe" a -tzip build\package\%name%\base\pak00.pk3 .\base\*
 "c:\Program Files\7-Zip\7z.exe" u -tzip build\package\%name%\base\pak00.pk3 .\%name%\*

6bc2332ea51eeee2e3de04ea964c7bcc931d8547
sponge <sponge@d8d.org>
2020-12-20 02:34:10 -0600

    premake and build tweaks

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,7 @@ newoption {
   default = false
 }
 
-solution "Slate2D"
+workspace "Slate2D"
   configurations { "Debug", "Release" }
   location "build"
   symbols "On"
@@ -24,24 +24,25 @@ solution "Slate2D"
   objdir "build/out/obj"
   startproject "wrengame"
 
-  filter { "system:windows" }
+  filter "system:windows"
     platforms { "x86", "x64" }
     systemversion "latest"
+    flags "MultiProcessorCompile"
 
-  filter { "system:macosx" }
+  filter "system:macosx"
     defines { "MACOS", "GL_SILENCE_DEPRECATION" }
-    platforms { "x64", "ARM" }
+    platforms { "x64", "arm64" }
 
-  filter { "system:linux" }
-    platforms { "x64" }
+  filter "system:linux"
+    platforms "x64"
     toolset "clang"
   
-  filter { "configurations:Debug" }
-    defines { "DEBUG" }
+  filter "configurations:Debug"
+    defines "DEBUG"
     optimize "Off"
 
-  filter { "configurations:Release" }
-    defines { "RELEASE" }
+  filter "configurations:Release"
+    defines "RELEASE"
     optimize "Full"
 
   project "libslate2d"
@@ -50,34 +51,34 @@ solution "Slate2D"
     language "C++"
     files { "src/**.c", "src/**.cpp", "src/**.h", "src/**.hh" }
     -- only used in emscripten build
-    removefiles { "src/imgui_impl_sdl_es2.cpp" }
+    removefiles "src/imgui_impl_sdl_es2.cpp"
     sysincludedirs { "src/external", "libs/sdl", "libs/tmx", "libs/imgui", "libs/physfs", "libs/glew", "libs/soloud/include", "libs/crunch" }
     -- physfs uses the exe path by default, but the game data files are in the top folder
     targetdir "build/bin/%{cfg.architecture}_%{cfg.buildcfg}"
     links { "tmx", "imgui", "physfs", "glew", "soloud", "crunch" }
     cppdialect "C++14"
     -- define so engine and emscripten packaging are always in sync on the same base game folder 
-    defines { "DEFAULT_GAME=\"" .. _OPTIONS["default-game"] .. "\"", "SLT_COMPILE_DLL"}
+    defines { "DEFAULT_GAME=\"" .. _OPTIONS["default-game"] .. "\"", "SLT_COMPILE_DLL" }
 
     -- rlgl and raymath have some warnings, suppress them here
-    filter { "files:src/glinit.c", "system:windows"}
+    filter { "files:src/glinit.c", "system:windows" }
       disablewarnings { 4204, 4100, 4267 }
 
     -- disable warnings for sds
-    filter { "files:src/external/sds.c"}
+    filter "files:src/external/sds.c"
       warnings "Off"
 
     -- compile all .c files in c mode so we don't get name mangling
-    filter { "files:**.c"}
+    filter "files:**.c"
       language "C"
 
     -- link SDL bins in the source tree on windows
-    filter { "system:windows" }
+    filter "system:windows"
       links { "SDL2", "opengl32" }
       defines { "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE" }
 
     filter { "platforms:x86", "system:windows" }
-      libdirs { "libs/sdl/lib/Win32" }
+      libdirs "libs/sdl/lib/Win32"
       -- need to copy x86 sdl runtime to output
       postbuildcommands {
         '{COPY} "%{wks.location}../libs/sdl/lib/Win32/SDL2.dll" "%{cfg.targetdir}" ',
@@ -85,7 +86,7 @@ solution "Slate2D"
       }
 
     filter { "platforms:x64", "system:windows" }
-      libdirs { "libs/sdl/lib/x64" }
+      libdirs "libs/sdl/lib/x64"
       -- need to copy x64 sdl runtime to output
       postbuildcommands {
         '{COPY} "%{wks.location}../libs/sdl/lib/x64/SDL2.dll" "%{cfg.targetdir}" ',
@@ -93,30 +94,33 @@ solution "Slate2D"
       }
 
     -- use SDL2 from homebrew
-    filter { "system:macosx" }
+    filter "system:macosx"
       links { "SDL2", "CoreFoundation.framework", "IOKit.framework", "CoreServices.framework", "Cocoa.framework", "OpenGL.framework" }
-      linkoptions {"-stdlib=libc++"}
+      linkoptions "-stdlib=libc++"
 
-    filter { "system:macosx", "platforms:arm" }
-      linkoptions {"-L /opt/homebrew/lib"}
+    filter { "system:macosx", "platforms:arm64" }
+      linkoptions "-L /opt/homebrew/lib"
 
     filter { "system:macosx", "platforms:x64" }
-      linkoptions {"-L /usr/local/lib"}
+      linkoptions "-L /usr/local/lib"
 
     -- emscripten uses opengl es2, not gl3
     filter { "action:gmake2", "options:emscripten" }
       targetextension ".bc"
-      files { "src/imgui_impl_sdl_es2.cpp" }
-      removefiles { "src/imgui_impl_sdl_gl3.cpp" }
+      files "src/imgui_impl_sdl_es2.cpp"
+      removefiles "src/imgui_impl_sdl_gl3.cpp"
       -- build the html bundle, overlay the default game folder on top of base
-      postbuildcommands { "mkdir html; emcc -O2 --preload-file ../base --preload-file ../" .. _OPTIONS["default-game"] .. " -s ALLOW_MEMORY_GROWTH=1 -s USE_SDL=2 %{cfg.targetdir}/engine.bc -o html/index.html --shell-file ../src/emshell.html" }
+      postbuildcommands {
+        "mkdir html", 
+        "emcc -O2 --preload-file ../base --preload-file ../" .. _OPTIONS["default-game"] .. " -s ALLOW_MEMORY_GROWTH=1 -s USE_SDL=2 %{cfg.targetdir}/engine.bc -o html/index.html --shell-file ../src/emshell.html",
+      }
       
-    filter { "system:linux" }
+    filter "system:linux"
       links { "SDL2", "dl", "pthread", "GL" }
 
-    filter { "options:static" }
+    filter "options:static"
       kind "StaticLib"
-      defines { "SLT_STATIC" }
+      defines "SLT_STATIC"
 
   project "wrengame"
     kind "ConsoleApp"
@@ -130,36 +134,36 @@ solution "Slate2D"
     links { "tmx", "imgui", "SDL2main", "libslate2d" }
 
     filter { "platforms:x86", "system:windows" }
-      libdirs { "libs/sdl/lib/Win32" }
+      libdirs "libs/sdl/lib/Win32"
 
     filter { "platforms:x64", "system:windows" }
-      libdirs { "libs/sdl/lib/x64" }
+      libdirs "libs/sdl/lib/x64"
 
-    filter { "system:windows" }
+    filter "system:windows"
       defines { "_CRT_SECURE_NO_WARNINGS", "_CRT_NONSTDC_NO_DEPRECATE" }
 
     -- use SDL2 from homebrew
-    filter { "system:macosx", "platforms:arm" }
-      linkoptions {"-stdlib=libc++", "-L /opt/homebrew/lib"}
+    filter { "system:macosx", "platforms:arm64" }
+      linkoptions {"-stdlib=libc++", "-L /opt/homebrew/lib" }
 
     filter { "system:macosx", "platforms:x64" }
-      linkoptions {"-stdlib=libc++", "-L /usr/local/lib"}
+      linkoptions {"-stdlib=libc++", "-L /usr/local/lib" }
 
     -- NaN tagging doesn't work in wren
     filter { "action:gmake2", "options:emscripten" }
       defines "WREN_NAN_TAGGING=0"
-      disablewarnings { "unknown-pragmas" }
+      disablewarnings "unknown-pragmas"
 
     -- disable warnings for wren code since it's external
     filter { "files:game/wren/* or files:game/wreninspector.cpp", "system:windows" }
       disablewarnings { 4100, 4200, 4996, 4244, 4204, 4702, 4709 }
 
     -- disable warnings for sds
-    filter { "files:../src/external/sds.c"}
+    filter "files:../src/external/sds.c"
       warnings "Off"
      
-    filter { "options:static" }
-      defines { "SLT_STATIC" }
+    filter "options:static"
+      defines "SLT_STATIC"
      
   group "libraries"
 
@@ -176,7 +180,7 @@ solution "Slate2D"
       files { "libs/imgui/**.cpp", "libs/imgui/**.h" }
       warnings "Off"
       
-      filter { "system:linux" }
+      filter "system:linux"
         pic "On"
 
     project "physfs"
@@ -186,15 +190,15 @@ solution "Slate2D"
       files { "libs/physfs/**.c", "libs/physfs/**.h", "libs/physfs/**.m" }
       warnings "Off"
 
-      filter { "system:macosx" }
-        undefines { "DEBUG" } -- fixes a weird issue on mac
+      filter "system:macosx"
+        undefines "DEBUG" -- fixes a weird issue on mac
 
     project "glew"
       language "C++"
       kind "StaticLib"
-      defines { "GLEW_STATIC" }
-      includedirs { "libs/glew" }
-      sysincludedirs { "libs/glew" }
+      defines "GLEW_STATIC"
+      includedirs "libs/glew"
+      sysincludedirs "libs/glew"
       files { "libs/glew/**.c", "libs/glew/**.h" }
       warnings "Off"
 
@@ -203,7 +207,7 @@ solution "Slate2D"
       kind "StaticLib"
       targetname "soloud_static"
       warnings "Off"
-      sysincludedirs { "libs/sdl" }
+      sysincludedirs "libs/sdl"
       defines { "MODPLUG_STATIC", "WITH_OPENMPT", "WITH_SDL2_STATIC" }
       files {
         "libs/soloud/src/audiosource/**.c*",

e5fbe64247485c6edb8e1a4ca069ceab5d45a46f
sponge <sponge@d8d.org>
2021-03-07 00:37:59 -0600

    finish out missing slate2d module funcs

--- jsgame/js_slt.cpp
+++ jsgame/js_slt.cpp
@@ -39,8 +39,7 @@ static JSValue js_slt_sndplay(JSContext *ctx, JSValueConst this_val, int argc, J
     if (JS_ToFloat64(ctx, &pan, argv[2])) return JS_EXCEPTION;
     if (JS_ToInt32(ctx, &loop, argv[3])) return JS_EXCEPTION;
 
-    SLT_Snd_Play(asset,volume,pan,loop);
-    return JS_UNDEFINED;
+    return JS_NewInt32(ctx, SLT_Snd_Play(asset,volume,pan,loop));
 }
 
 static JSValue js_slt_sndstop(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
@@ -64,7 +63,16 @@ static JSValue js_slt_sndpauseresume(JSContext *ctx, JSValueConst this_val, int 
 }
 
 static JSValue js_slt_registerbuttons(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
-    //SLT_In_AllocateButtons();
+    if (!JS_IsArray(ctx, argv[0])) return JS_EXCEPTION;
+    int len = 0;
+    JS_ToInt32(ctx, &len, JS_GetPropertyStr(ctx, argv[0], "length"));
+    const char **strings = (const char **)malloc(sizeof(void*) * len);
+    for (int i = 0; i < len; i++) {
+        JSValueConst val = JS_GetPropertyUint32(ctx, argv[0], i);
+        strings[i] = JS_ToCString(ctx, val);
+    }
+    SLT_In_AllocateButtons(&strings[0], len);
+    free((void*)strings);
     return JS_UNDEFINED;
 }
 
@@ -77,8 +85,7 @@ static JSValue js_slt_buttonpressed(JSContext *ctx, JSValueConst this_val, int a
     if (JS_ToInt32(ctx, &delay, argv[1])) return JS_EXCEPTION;
     if (JS_ToInt32(ctx, &repeat, argv[2])) return JS_EXCEPTION;
 
-    SLT_In_ButtonPressed(buttonNum,delay,repeat);
-    return JS_UNDEFINED;
+    return JS_NewBool(ctx, SLT_In_ButtonPressed(buttonNum,delay,repeat));
 }
 
 static JSValue js_slt_setwindowtitle(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
@@ -90,16 +97,48 @@ static JSValue js_slt_setwindowtitle(JSContext *ctx, JSValueConst this_val, int 
     return JS_UNDEFINED;
 }
 
+#if defined(_WIN32)
+static const char *platform = "windows";
+#elif defined(MACOS)
+static const char *platform = "macos";
+#elif defined(__EMSCRIPTEN__)
+static const char *platform = "emscripten";
+#else
+static const char *platform = "unknown";
+#endif
+
+static JSValue js_slt_getmouse(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+    MousePosition pos = SLT_In_MousePosition();
+    JSValue obj = JS_NewObject(ctx);
+    JS_SetPropertyStr(ctx, obj, "x", JS_NewInt32(ctx, pos.x));
+    JS_SetPropertyStr(ctx, obj, "y", JS_NewInt32(ctx, pos.y));
+
+    return obj;
+}
+
+static JSValue js_slt_getresolution(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) {
+    int w, h; 
+    SLT_GetResolution(&w, &h);
+    JSValue obj = JS_NewObject(ctx);
+    JS_SetPropertyStr(ctx, obj, "w", JS_NewInt32(ctx, w));
+    JS_SetPropertyStr(ctx, obj, "h", JS_NewInt32(ctx, h));
+
+    return obj;
+}
+
 static const JSCFunctionListEntry js_slt_funcs[] = {
-        JS_CFUNC_DEF("printWin", 0, js_slt_printwin),
+        JS_CFUNC_DEF("printWin", 1, js_slt_printwin),
         JS_CFUNC_DEF("error", 2, js_slt_error),
         JS_CFUNC_DEF("console", 1, js_slt_console),
         JS_CFUNC_DEF("sndPlay", 4, js_slt_sndplay),
         JS_CFUNC_DEF("sndStop", 1, js_slt_sndstop),
         JS_CFUNC_DEF("sndPauseResume", 2, js_slt_sndpauseresume),
-        JS_CFUNC_DEF("registerButtons", 0, js_slt_registerbuttons),
+        JS_CFUNC_DEF("registerButtons", 1, js_slt_registerbuttons),
         JS_CFUNC_DEF("buttonPressed", 3, js_slt_buttonpressed),
         JS_CFUNC_DEF("setWindowTitle", 1, js_slt_setwindowtitle),
+        JS_PROP_STRING_DEF("platform", platform, 0),
+        JS_CFUNC_DEF("mouse", 0, js_slt_getmouse),
+        JS_CFUNC_DEF("resolution", 0, js_slt_getresolution),
 };
 
 static int js_slt_init(JSContext *ctx, JSModuleDef *m)
--- plat/main.js
+++ plat/main.js
@@ -1,17 +1,28 @@
 import { testmodule } from "testmodule.js";
 import * as Draw from "draw";
+import * as SLT from "slate2d";
 
 globalThis.start = function() {
   console.log("start");
+  console.log("platform is " + SLT.platform);
+  SLT.registerButtons(['up', 'down', 'left', 'right']);
 };
 
 let t = 0;
 globalThis.draw = function() {
-  let x = (t * 100) % 800;
-  let y = Math.sin(x / 50) * 100 + 200;
-  let sz = Math.cos(t) * 32 + 32
+  const mouse = SLT.mouse();
+  const res = SLT.resolution();
+
+  const x = (t * 100) % res.w;
+  const y = Math.sin(x / 50) * 100 + 200;
+  const sz = Math.cos(t) * 32 + 32
   Draw.rect(x, y, sz, sz, true);
+
   Draw.tri(10, 20, 400, 400, 400, 100, true);
+  Draw.setColor(128, 0, 0, 255);
+  Draw.line(0, 0, res.w, res.h);
+  Draw.setColor(255, 255, 255, 128);
+  Draw.rect(mouse.x, mouse.y, 16, 16);
   Draw.submit();
 };
 

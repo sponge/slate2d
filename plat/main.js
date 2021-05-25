f8a9110ec9e9e9a4d961d148eb655fb3b93e98f1
sponge <sponge@d8d.org>
2021-05-25 00:07:30 -0500

    parallax bg and the dog now romps through the grass

--- plat/main.js
+++ plat/main.js
@@ -13,6 +13,8 @@ class Main {
   test2 = "hello world";
   test3 = new Promise(() => {}, () => {});
   tiles = undefined;
+  backgrounds = [];
+  clouds = [];
   
   constructor() {
     console.log("constructed!");
@@ -53,31 +55,58 @@ class Main {
     });
 
     this.tiles = loadTilemap('maps/8x8.json');
+
+    this.backgrounds = [...Array(3).keys()].map(i => {
+      const name = `gfx/grassland_bg${i}.png`;
+      const id = Assets.load({type: 'image', name, path: name});
+      const {w, h} = Assets.imageSize(id);
+      return { id, w, h };
+    });
+
+    const cloudX = [50, 100, 150];
+    const cloudY = [15, 45, 30];
+    this.clouds = [...Array(3).keys()].map(i => {
+      const name = `gfx/grassland_cloud${i}.png`;
+      const id = Assets.load({type: 'image', name, path: name});
+      const {w, h} = Assets.imageSize(id);
+      return { id, w, h, x: cloudX[i], y: cloudY[i] };
+    });
+
   };
 
   draw() {
     Draw.clear(0, 0, 0, 255);
 
     Draw.useCanvas(this.canvas);
-    Draw.clear(0, 0, 0, 255);
+    Draw.clear(41, 173, 255, 255);
   
     const mouse = SLT.mouse();
     const res = {w: 384, h: 216};
   
     let t = this.state.t;
   
-    const x = Math.floor((t * 50) % res.w);
-    const y = Math.floor(Math.sin(x / 50) * 100 + 80);
-    const sz = Math.cos(t) * 9 + 32
-    Draw.rect(x, y, sz, sz, true);
+    this.backgrounds.forEach((bg, i) => {
+      const speed = (i+1) * 32;
+      const x = (0 - t * speed) % res.w;
+      Draw.image(bg.id, x, res.h - bg.h, 0, 0, 1, 0, 0, 0);
+      Draw.image(bg.id, x + bg.w, res.h - bg.h, 0, 0, 1, 0, 0, 0);
+    });
 
+    this.clouds.forEach((bg, i) => {
+      const speed = (i+1) * 6;
+      const x = res.w + (bg.x - t * speed) % (res.w + bg.w);
+      Draw.image(bg.id, x, bg.y, 0, 0, 1, 0, 0, 0);
+    });
+  
+    const x = Math.floor((t * 50) % (res.w + 22) - 22);
+    const y = Math.floor(Math.sin(x / 50) * 5 + 167);
+    const sz = Math.cos(t) * 9 + 32
     Draw.setColor(255, 255, 255, 255);
-    Draw.sprite(this.dogSpr, Math.floor(t * 8) % 6, x, y, 2, 0, 1, 1);
+    Draw.sprite(this.dogSpr, Math.floor(t * 12) % 6, x, y, 1, 0, 1, 1);
     Draw.setColor(255, 255, 255, 128);
   
-    Draw.tilemap(this.tiles.tilesetHandle, 0, 0, this.tiles.width, this.tiles.height, this.tiles.data);
+    //Draw.tilemap(this.tiles.tilesetHandle, 0, 0, this.tiles.width, this.tiles.height, this.tiles.data);
 
-    Draw.image(this.dog, 0, 0, 32, 32, 1, 0, 0, 0);
     Draw.tri(10, 20, 400, 400, 400, 100, true);
     Draw.setColor(128, 0, 0, 255);
     Draw.line(0, 0, res.w, res.h);

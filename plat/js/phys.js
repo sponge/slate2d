80922c7b9d60edc3865bda0c5f3eae6aac66f30a
sponge <sponge@d8d.org>
2021-06-23 00:17:05 -0500

    fix phys constants to 16 width tiles

--- plat/js/phys.js
+++ plat/js/phys.js
@@ -2,28 +2,28 @@ const Phys = {
     // physics values
     // most values from https://cdn.discordapp.com/attachments/191015116655951872/332350193540268033/smw_physics.png
     pMeterCapacity: 112,
-    friction: 0.03125 * 2,
-    accel: 0.046875 * 2,
-    skidAccel: 0.15625 * 2,
-    runSpeed: 1.125 * 2,
-    maxSpeed: 1.5 * 2,
-    heldGravity: 0.1875 / 2 * 2,
-    gravity: 0.1875 * 2,
+    friction: 0.0625,
+    accel: 0.09375,
+    skidAccel: 0.3125,
+    runSpeed: 2.25,
+    maxSpeed: 3,
+    heldGravity: 0.1875,
+    gravity: 0.375,
     earlyBounceFrames: 8,
     earlyJumpFrames: 6,
     lateJumpFrames: 6,
-    terminalVelocity: 2 * 2,
-    enemyJumpHeld: 3.4 * 2,
-    enemyJump: 1.9 * 2,
+    terminalVelocity: 4,
+    enemyJumpHeld: 5.5,
+    enemyJump: 3,
     slideSpeed: 4,
     jumpHeights: [
-        [3, 2.875 * 2],
-        [2.5, 2.78125 * 2],
-        [2, 2.71875 * 2],
-        [1.5, 2.625 * 2],
-        [1, 2.5625 * 2],
-        [0.5, 2.46875 * 2],
-        [0, 2.40625 * 2]
+        [3, 5.75],
+        [2.5, 5.5625],
+        [2, 5.4375],
+        [1.5, 5.25],
+        [1, 5.125],
+        [0.5, 4.9375],
+        [0, 4.8125]
     ]
 };
 export default Phys;

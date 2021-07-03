c03f2643435a19329e0a6502fed34fce1f46ba3b
sponge <sponge@d8d.org>
2021-07-02 23:01:15 -0500

    start movin the bird back and forth based on the map editor

--- plat/js/entities/bird.js
+++ plat/js/entities/bird.js
@@ -9,21 +9,36 @@ class Bird extends Entity {
     sprite = Assets.find('bird');
     enabled = true;
     delta = [0, 0];
+    dir = [0, 0];
+    moveAmt = [0, 0];
     constructor(args) {
         super(args);
         // points are kinda hard to work with and they're not in any format
         // that maps cleanly to the entity position
         const dest = args.properties?.Destination ?? this.pos;
         const tilePos = [Math.floor(this.pos[0] / 16), Math.floor(this.pos[1] / 16)];
+        console.log(tilePos);
         this.delta[0] = (dest[0] - tilePos[0]) * 16;
         this.delta[1] = (dest[1] - tilePos[1]) * 16;
+        const absDelta = [Math.abs(this.delta[0]), Math.abs(this.delta[1])];
+        let dx = absDelta[0] > absDelta[1] ? 1 : absDelta[0] / absDelta[1];
+        let dy = absDelta[1] > absDelta[0] ? 1 : absDelta[1] / absDelta[0];
+        dx *= Math.sign(this.delta[0]) * 0.5;
+        dy *= Math.sign(this.delta[1]) * 0.5;
+        this.dir = [dx, dy];
     }
     update(ticks, dt) {
         this.frame = Math.floor(ticks / 8 % 4);
         this.frame = this.frame == 3 ? 1 : this.frame;
-        const dir = [Math.sign(this.delta[0]), Math.sign(this.delta[1])];
-        this.moveX(dir[0]);
-        this.moveY(dir[1]);
+        this.moveSolid(this.dir[0], this.dir[1]);
+        this.moveAmt[0] += this.dir[0];
+        this.moveAmt[1] += this.dir[1];
+        const dim = this.delta[0] > this.delta[1] ? 0 : 1;
+        if (Math.abs(this.moveAmt[dim]) >= Math.abs(this.delta[dim])) {
+            this.dir[0] *= -1;
+            this.dir[1] *= -1;
+            this.moveAmt = [0, 0];
+        }
     }
     draw() {
         SLT.showObj('bird', this);
--- plat/ts/entities/bird.ts
+++ plat/ts/entities/bird.ts
@@ -15,6 +15,8 @@ class Bird extends Entity {
   sprite = Assets.find('bird');
   enabled = true;
   delta: [number, number] = [0, 0];
+  dir: [number, number] = [0, 0];
+  moveAmt: [number, number] = [0, 0];
 
   constructor(args: { [key: string]: any }) {
     super(args);
@@ -23,17 +25,35 @@ class Bird extends Entity {
     // that maps cleanly to the entity position
     const dest = args.properties?.Destination ?? this.pos;
     const tilePos = [Math.floor(this.pos[0] / 16), Math.floor(this.pos[1] / 16)];
+    console.log(tilePos);
     this.delta[0] = (dest[0] - tilePos[0]) * 16;
     this.delta[1] = (dest[1] - tilePos[1]) * 16;
+
+    const absDelta = [Math.abs(this.delta[0]), Math.abs(this.delta[1])];
+
+    let dx = absDelta[0] > absDelta[1] ? 1 : absDelta[0] / absDelta[1];
+    let dy = absDelta[1] > absDelta[0] ? 1 : absDelta[1] / absDelta[0];
+
+    dx *= Math.sign(this.delta[0]) * 0.5;
+    dy *= Math.sign(this.delta[1]) * 0.5;
+
+    this.dir = [dx, dy];
   }
 
   update(ticks: number, dt: number) {
     this.frame = Math.floor(ticks / 8 % 4);
     this.frame = this.frame == 3 ? 1 : this.frame;
 
-    const dir = [Math.sign(this.delta[0]), Math.sign(this.delta[1])];
-    this.moveX(dir[0]);
-    this.moveY(dir[1]);
+    this.moveSolid(this.dir[0], this.dir[1]);
+    this.moveAmt[0] += this.dir[0];
+    this.moveAmt[1] += this.dir[1]
+
+    const dim = this.delta[0] > this.delta[1] ? 0 : 1;
+    if (Math.abs(this.moveAmt[dim]) >= Math.abs(this.delta[dim])) {
+      this.dir[0] *= -1;
+      this.dir[1] *= -1;
+      this.moveAmt = [0, 0];
+    }
   }
 
   draw() {

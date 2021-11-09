afdd14fa301be5d73f708fcda33ff74037829ba9
sponge <sponge@d8d.org>
2021-11-09 00:37:26 -0600

    default main.js and xcopy base to bin so it runs something out of the box

---
+++ base/js/main.js
@@ -0,0 +1,23 @@
+import * as SLT from 'slate2d';
+import * as Draw from 'draw';
+
+class Main {
+  save() {
+    return [];
+  }
+
+  constructor(initialState) {
+      
+  }
+
+  update(dt) {
+      
+  }
+
+  draw() {
+    Draw.clear(0, 0, 0, 255);
+    Draw.rect(128, 128, 64, 64, false);
+  }
+}
+
+export default Main;

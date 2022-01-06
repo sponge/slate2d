07f02be87a8660adced018ad0c11ea3bb5a772e0
sponge <sponge@d8d.org>
2022-01-05 22:16:38 -0600

    fix packing script

--- package.bat
+++ package.bat
@@ -4,7 +4,7 @@ set name=%1
 if exist build\package rmdir /S /Q build\package
 
 mkdir build\package\%name%\base
-copy build\bin\x86_64_Release\slate2d.exe build\package\%name%\%name%.exe
+copy build\bin\x86_64_Release\jslate2d.exe build\package\%name%\%name%.exe
 copy build\bin\x86_64_Release\slate2d.dll build\package\%name%\
 copy build\bin\x86_64_Release\SDL2.dll build\package\%name%\
 copy build\bin\x86_64_Release\libopenmpt.dll build\package\%name%\
@@ -17,6 +17,6 @@ copy build\bin\x86_64_Release\game.dll build\package\%name%\base\
 if exist %name%\icon.ico rcedit-x64 "build\package\%name%\%name%.exe" --set-icon "%name%\icon.ico"
 
 "c:\Program Files\7-Zip\7z.exe" a -tzip build\package\%name%\base\pak00.pk3 .\base\*
-"c:\Program Files\7-Zip\7z.exe" u -tzip build\package\%name%\base\pak00.pk3 .\%name%\*
+"c:\Program Files\7-Zip\7z.exe" a -tzip build\package\%name%\base\pak00.pk3 .\%name%\*
 
 "c:\Program Files\7-Zip\7z.exe" a -tzip build\%name%.zip .\build\package\*

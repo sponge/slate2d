c560b56f80c179d2fb64853953df64f0054684b9
sponge <sponge@d8d.org>
2023-02-12 00:39:19 -0600

    finish implementing coin

--- csgame/entities/Coin.cs
+++ csgame/entities/Coin.cs
@@ -5,6 +5,21 @@ class Coin : Entity
 {
     public Coin(LDTKEntity ent) : base(ent)
     {
+        Collidable = CollisionType.Trigger;
         Sprite = Assets.Find("coin");
+        Layer = Layer.Background;
+    }
+
+    public override void Collide(Entity other, Dir dir)
+    {
+        if (other is Player == false) return;
+
+        Destroyed = true;
+        Main.World.GameState.CurrentCoins++;
+    }
+
+    public override void Update(uint ticks, float dt)
+    {
+        Frame = ticks / 8 % 4;
     }
 }
